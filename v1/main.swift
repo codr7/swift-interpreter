ddea69095a13c8b2e9a95de2a8e83d228ca392d1
codr7 <codr7@protonmail.com>
2023-10-01 22:58:01 +0200

    Fix wording and formatting.

--- v1/main.swift
+++ v1/main.swift
@@ -1,5 +1,4 @@
 /***
-
  The goal here is to provide the foundation for an interpreter in Swift that is simple enough to understand,
  yet substantial enough to continue building upon and playing around with.
 
@@ -8,14 +7,11 @@
  Rather than starting with syntax and parsers, we'll work from the bottom up.
  
  To try it out; simply download and install Swift, and run `swift main.swift`.
-
  ***/
 
 /*
-
  First out is types and values.
  Types don't do much yet, but may be used to specialize behavior for certain kinds of values.
- 
  */
 
 class ValType {
@@ -41,16 +37,21 @@ struct Val {
 }
 
 /*
-
- The type alias is just a way to make it clear we're dealing with a program counter,
+ We'll use a type alias to make it clear when we're dealing with a program counter,
  as opposed to any random integer.
- 
- Functions take the current program counter as a reference argument,
- which allows using the same type and operations for primitives and user functions etc.
- 
  */
 
 typealias Pc = Int
+
+/*
+ Functions take the current program counter as a reference argument,
+ which allows them to decide where to return to.
+
+ This time around, we'll only deal with primitives, that have no virtual code;
+ which means that they'll typically simply increase the program counter.
+ 
+ But once we get to user defined functions, this allows us to jump to the actual code of the called function.
+ */
 
 class Fun {
     typealias Body = (M, inout Pc) throws -> Void
@@ -69,14 +70,12 @@ class Fun {
 }
 
 /*
-
- Operations are the things that our virtual machine will execute.
+ Operations are the things that our virtual machine executes.
 
  Any kind of code we want to run on it, regardless of syntax; needs to be reduced to a sequence of operations.
 
  The reason there's a separate case for stopping execution is to avoid having to check in the eval loop,
  which needs to be as fast as possible.
- 
  */
 
 enum Op {
@@ -86,9 +85,7 @@ enum Op {
 }
 
 /*
-
  The virtual machine is where the rubber finally meets the road.
- 
  */
 
 class M {
@@ -132,13 +129,14 @@ class M {
 }
 
 /*
-
  Now we're ready to take it for a spin.
+
  We'll settle for some simple arithmetics this time around, just to get an idea how everything works.
- 
  */
 
 let m = M()
+
+let intType = ValType("Int")
 
 let addFun = Fun("+") {(m: M, returnPc: inout Pc) throws -> Void in
     let r = m.pop()!
@@ -146,8 +144,6 @@ let addFun = Fun("+") {(m: M, returnPc: inout Pc) throws -> Void in
     m.push(Val(intType, (l.data as! Int) + (r.data as! Int)))
     returnPc += 1
 }
-
-let intType = ValType("Int")
 
 m.emit(.push(Val(intType, 6)))
 m.emit(.push(Val(intType, 4)))
@@ -157,7 +153,7 @@ m.emit(.stop)
 try m.eval(fromPc: 0)
 
 /*
- This prints [10], which is the final stack contents after adding 6 to 4.
+ This prints [10], which is the final contents of the stack after adding 6 to 4.
  */
 
 print(m.dumpStack())
