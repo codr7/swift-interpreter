cef2fa282da3b6e4056880a33f3e384ba252b1f2
codr7 <codr7@protonmail.com>
2023-11-28 21:29:31 +0100

    Combine benchmarks.

--- part7/benchmarks/fib.py
+++ part7/benchmarks/fib.py
@@ -1,5 +1,13 @@
 from benchmark import benchmark
 
+print(benchmark(100, '''def fib(n):
+  return n if n < 2 else fib(n-1) + fib(n-2)
+''', '''fib(20)'''))
+
+print(benchmark(10000, '''def fib(n, a, b):
+  return a if n == 0 else b if n == 1 else fib(n-1, b, a+b)
+''', '''fib(70, 0, 1)'''))
+
 print(benchmark(10000, '''
 def fib(n, lookup):
   if n in lookup: return lookup[n]
--- part7/benchmarks/fib2.py
+++
@@ -1,5 +0,0 @@
-from benchmark import benchmark
-
-print(benchmark(10000, '''def fib(n, a, b):
-  return a if n == 0 else b if n == 1 else fib(n-1, b, a+b)
-''', '''fib(70, 0, 1)'''))
