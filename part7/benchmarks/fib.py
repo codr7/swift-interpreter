2c8978fc7d72ae55fd22b62620e26e98129ed79b
codr7 <codr7@protonmail.com>
2023-11-29 05:15:44 +0100

    Formatting.

--- part7/benchmarks/fib.py
+++ part7/benchmarks/fib.py
@@ -1,8 +1,10 @@
 from benchmark import benchmark
 
-print(benchmark(100, '''def fib(n):
+print(benchmark(100, '''
+def fib(n):
   return n if n < 2 else fib(n-1) + fib(n-2)
-''', '''fib(20)'''))
+''',
+'fib(20)'))
 
 print(benchmark(10000, '''
 def fib(n, lookup):
@@ -11,8 +13,10 @@ def fib(n, lookup):
   lookup[n] = result
   return result
 ''',
-'''fib(70, {})'''))
+'fib(70, {})'))
 
-print(benchmark(10000, '''def fib(n, a, b):
+print(benchmark(10000, '''
+def fib(n, a, b):
   return a if n == 0 else b if n == 1 else fib(n-1, b, a+b)
-''', '''fib(70, 0, 1)'''))
+''',
+'fib(70, 0, 1)'))
