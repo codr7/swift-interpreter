3fa20f0a76bcb157bc9597873d2f296c9fef179a
codr7 <codr7@protonmail.com>
2023-12-01 12:15:50 +0100

    Finished AOC 1 & 2.

---
+++ part10/aoc/code1.lisp
@@ -0,0 +1,20 @@
+(defun read-lines (path)
+  (with-open-file (f path)
+    (labels ((rec (out)
+	       (let ((line (read-line f nil)))
+		 (if line
+		     (rec (cons line out))
+		     (nreverse out)))))
+      (rec nil))))
+
+(defun find-digit (in &key (from-end nil))
+  (let ((c (find-if #'digit-char-p in :from-end from-end)) )
+    (unless c
+      (error "No digits found"))
+    (- (char-code c) (char-code #\0))))
+   
+(defun decode-line (line)
+  (parse-integer (format nil "~a~a" (find-digit line) (find-digit line :from-end t))))
+   
+(defun calibrate (path)
+  (reduce #'+ (mapcar #'decode-line (read-lines path))))
---
+++ part10/aoc/code2.lisp
@@ -0,0 +1,37 @@
+(defun read-lines (path)
+  (with-open-file (f path)
+    (labels ((rec (out)
+	       (let ((line (read-line f nil)))
+		 (if line
+		     (rec (cons line out))
+		     (nreverse out)))))
+      (rec nil))))
+
+(defun find-string (in strings &key (from-end nil))
+  (let ((ri (if from-end -1 (length in))) rv (v 1))
+    (dolist (s strings)
+      (let ((i (search s in :from-end from-end)))
+	(when (and i (or (and from-end (> i ri))
+			 (and (not from-end) (< i ri))))
+	  (setf ri i rv v)))
+      (incf v))
+    (values ri rv)))
+
+(defun find-char-digit (in &key (from-end nil))
+  (find-string in '("1" "2" "3" "4" "5" "6" "7" "8" "9") :from-end from-end))
+
+(defun find-word-digit (in &key (from-end nil))
+  (find-string in '("one" "two" "three" "four" "five" "six" "seven" "eight" "nine") :from-end from-end))
+
+(defun find-digit (in &key (from-end nil))
+  (multiple-value-bind (ci cv) (find-char-digit in :from-end from-end)
+    (multiple-value-bind (wi wv) (find-word-digit in :from-end from-end)
+      (if from-end
+	  (if (> ci wi) cv wv)
+	  (if (< ci wi) cv wv)))))
+
+(defun decode-line (line)
+  (parse-integer (format nil "~a~a" (find-digit line) (find-digit line :from-end t))))
+
+(defun calibrate (path)
+  (reduce #'+ (mapcar #'decode-line (read-lines path))))
